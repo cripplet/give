b4f038b5d988eb503ed33d03283d4b1b63d8af47
Minke Zhang <minke.zhang@gmail.com>
2018-07-02 01:59:56 +0000

    Refactor unit test for getJWTCredentials

--- backend/test/user.test.ts
+++ backend/test/user.test.ts
@@ -12,11 +12,17 @@ import * as user from "../src/models/user";
 
 admin.initializeApp(config.config("dev").adminAppConfig);
 
-const userDetails = {
+const testUser = {
   username: "testUser",
   email: "some.test@gmail.com",
   phone: "+15555555555"
 };
+
+interface UserDetails {
+  username: string;
+  email: string;
+  phone: string;
+}
 
 interface VerifyCustomTokenResponse {
   kind: string;
@@ -25,9 +31,55 @@ interface VerifyCustomTokenResponse {
   expiresIn: string;
 }
 
+const createAndGetIDToken = (userDetails: UserDetails): Observable<string> => {
+  return from(
+    admin.auth().createUser({
+      disabled: false, // allow sign-in
+      displayName: userDetails.username,
+      email: userDetails.email,
+      emailVerified: false,
+      phoneNumber: userDetails.phone
+    })
+  ).pipe(
+    map((userRecord: admin.auth.UserRecord): string => userRecord.uid),
+    flatMap(
+      (uid: string): Observable<string> =>
+        from(admin.auth().createCustomToken(uid))
+    ),
+    flatMap(
+      (customToken: string): Observable<VerifyCustomTokenResponse> =>
+        from(
+          request({
+            // sign in as the user to get ID token
+            method: "POST",
+            uri:
+              "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken",
+            qs: {
+              key: config.config("dev").clientAppConfig.apiKey
+            },
+            headers: {
+              "Content-Type": "application/json"
+            },
+            json: true,
+            body: {
+              token: customToken,
+              returnSecureToken: true
+            }
+          })
+        )
+    ),
+    map(
+      (response: VerifyCustomTokenResponse): string => {
+        expect(response.idToken).toBeTruthy();
+        return response.idToken;
+      }
+    )
+  );
+};
+
 describe("Test Admin SDK User management", (): void => {
   beforeEach((done: jest.DoneCallback): void => {
-    const observable: Observable<void> = of(userDetails.email).pipe(
+    const observable: Observable<void> = of(testUser.email).pipe(
       flatMap(
         (email: string): Observable<admin.auth.UserRecord> =>
           from(admin.auth().getUserByEmail(email))
@@ -42,58 +94,26 @@ describe("Test Admin SDK User management", (): void => {
       complete: done
     });
   });
+
   test("getCorrectJWTCredentials", (done: jest.DoneCallback): void => {
-    const observable: Observable<string> = from(
-      admin.auth().createUser({
-        disabled: false, // allow sign-in
-        displayName: userDetails.username,
-        email: userDetails.email,
-        emailVerified: false,
-        phoneNumber: userDetails.phone
-      })
-    ).pipe(
-      map((userRecord: admin.auth.UserRecord): string => userRecord.uid),
-      flatMap(
-        (uid: string): Observable<string> =>
-          from(admin.auth().createCustomToken(uid))
-      ),
-      flatMap(
-        (customToken: string): Observable<VerifyCustomTokenResponse> =>
-          from(
-            request({
-              // sign in as the user to get ID token
-              method: "POST",
-              uri:
-                "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken",
-              qs: {
-                key: config.config("dev").clientAppConfig.apiKey
-              },
-              headers: {
-                "Content-Type": "application/json"
-              },
-              json: true,
-              body: {
-                token: customToken,
-                returnSecureToken: true
-              }
-            })
-          )
-      ),
-      map(
-        (response: VerifyCustomTokenResponse): string => {
-          expect(response.idToken).toBeTruthy();
-          return response.idToken;
-        }
-      ),
-      flatMap(
-        (idToken: string): Observable<string> => user.getJWTCredentials(idToken)
-      )
-    );
-    observable.subscribe(
+    const observable: Observable<string> = createAndGetIDToken(testUser);
+    observable.pipe(
+      flatMap((idToken: string): Observable<string> => user.getJWTCredentials(idToken))
+    ).subscribe(
       (customToken: string): void => {
         expect(customToken).toBeTruthy();
         done();
       }
+    );
+  });
+
+  test("errorWithInvalidIDToken", (done: jest.DoneCallback): void => {
+    const observable: Observable<string> = from(user.getJWTCredentials("some-invalid-string"))
+    observable.subscribe(
+      (customToken: string): void => {
+        done.fail("Got generated custom credentials for an invalid user.");
+      },
+      (err: Error): void => { done(); },
     );
   });
 });
